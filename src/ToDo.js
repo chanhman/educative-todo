a3aca441bff9b866bf9ee0c49372090c5272fb1f
Chanh Nguyen <hello@chanh.design>
2020-12-25 14:22:21 -0700

    toggle todo's

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-export default function ToDo({ toDo }) {
-  return <div>{toDo.task}</div>
+export default function ToDo({ toDo, handleToggle }) {
+  const handleClick = (e) => {
+    e.preventDefault()
+    handleToggle(e.currentTarget.id)
+  }
+  return (
+    <div
+      id={toDo.id}
+      className={toDo.complete ? 'strike' : ''}
+      onClick={handleClick}
+    >
+      {toDo.task}
+    </div>
+  )
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ToDo from './ToDo'
 
-export default function ToDoList({ toDoList }) {
+export default function ToDoList({ toDoList, handleToggle, handleFilter }) {
   return (
     <div>
       {toDoList.map((toDo) => {
-        return <ToDo key={toDo.id} toDo={toDo} />
+        return (
+          <ToDo
+            key={toDo.id}
+            handleToggle={handleToggle}
+            handleFilter={handleFilter}
+            toDo={toDo}
+          />
+        )
       })}
+      <button onClick={handleFilter}>Clear</button>
     </div>
   )
 }
